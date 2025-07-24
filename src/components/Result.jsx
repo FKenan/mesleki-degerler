301028e4447c8a6c608408aeee3ff4c647a7102f
FKenan <kenanf734@gmail.com>
2025-07-24 19:17:58 +0300

    Add ResultPage component with DataGrid for displaying data

---
+++ src/components/Result.jsx
@@ -0,0 +1,43 @@
+const paginationModel = { page: 0, pageSize: 5 };
+
+const columns = [
+  // Sample data
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 90,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "asdf", firstName: "dsfsd", age: 35 },
+  { id: 2, lastName: "asdf", firstName: "dfsa", age: 42 },
+  // tablo satırları
+];
+
+export default function ResultPage() {
+  return (
+    <Paper sx={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+        sx={{ border: 0 }}
+      />
+    </Paper>
+  );
+}
